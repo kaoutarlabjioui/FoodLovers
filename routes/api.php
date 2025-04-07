68be491e5ce4ee2cff9e78988b9425e2982ed387
kaoutarlabjioui <kalabjioui@gmail.com>
2025-04-07 01:21:58 +0100

    register , login , logout acev jwt auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// user
+Route::post('/login',  [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware("jwtAuth");
+Route::post('/refresh', [AuthController::class, 'refresh'])->middleware("jwtAuth");
+Route::get('/user-profile', [AuthController::class, 'getUser'])->middleware("jwtAuth");
